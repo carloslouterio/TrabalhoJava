7e6bd7166eefd43d6baa86c3457a5e9daa26c10c
Carlos Luiz <carloslouterio15@gmail.com>
2016-04-26 19:27:26 -0300

    INCLLUINDO CODIGO EM COLUNA

--- src/br/univel/anotacao/Coluna.java
+++ src/br/univel/anotacao/Coluna.java
@@ -1,5 +1,15 @@
 package br.univel.anotacao;
 
-public @interface Coluna {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Coluna {
+	
+	String nome() default "";
+	boolean pk() default false;
+  
 }
