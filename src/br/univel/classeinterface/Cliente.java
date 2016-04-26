6ce4790eaa3e84d34ba4a55b688870dca9dc6e4f
Carlos Luiz <carloslouterio15@gmail.com>
2016-04-26 19:41:36 -0300

    Cleinte finalizado

--- src/br/univel/classeinterface/Cliente.java
+++ src/br/univel/classeinterface/Cliente.java
@@ -6,7 +6,7 @@ import enums.EstadoCivil;
 
 @Tabela("CAD_CLIENTE")
 public class Cliente {
-
+	
 	@Coluna(pk=true)
 	private int id;
 	
@@ -19,7 +19,7 @@ public class Cliente {
 	
 	public Cliente(){
 		this(0, null, null, null, null);
-}
+	}
 	
 	public Cliente(int id, String nome, String endereco, String telefone, EstadoCivil estadocivil){
 		super();
@@ -29,3 +29,52 @@ public class Cliente {
 		this.telefone = telefone;
 		this.estadocivil = estadocivil;
 	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public EstadoCivil getEstadocivil() {
+		return estadocivil;
+	}
+
+	public void setEstadocivil(EstadoCivil estadocivil) {
+		this.estadocivil = estadocivil;
+	}
+
+	@Override
+	public String toString(){
+		return "Cliente [id=" + id + ", nome=" + nome + ", endereco=" + endereco + ", telefone=" + telefone 
+				+ ", Estado Civil=" + estadocivil;
+	}
+	
+	
+}
